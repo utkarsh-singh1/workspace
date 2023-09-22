8781e20accd766861c103d67e98be936678cf30a
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2023-09-22 17:19:34 +0530

    Added new files in repo
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

---
+++ Go-workspace/src/Go-training/01_hello_world.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 56
+	var y string = "Hariom"
+
+	fmt.Printf("%s is %#v years old \n",y,x)
+
+	{
+
+		x := true
+
+		fmt.Printf("%t",x)
+		
+	}
+} 
