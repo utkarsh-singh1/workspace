3cfdb737512225aab7dda023523ddc345c3916e8
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2023-12-06 23:45:13 +0530

    Added new folder "Loop", be in loop
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

---
+++ Go-workspace/src/Go-training/06_Loop/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// for init ; condition ; post {}
+	
+	x := 1
+
+	for  x*15 < 1000 {
+		fmt.Printf("Loop will run once again because value of x is %d\n",x)
+		x++
+	}
+
+	// range in for
+
+	y := [5]int{1,2,3,4,5}
+
+	z := [5]int{}
+
+	fmt.Printf("\n")
+
+	for key , value := range y {
+		z[key] = value+3
+	}
+
+	fmt.Println("index No.","value")
+
+	for i,j := range z {
+		
+		fmt.Println(" ",i,"     |  ",j)
+	}
+}
