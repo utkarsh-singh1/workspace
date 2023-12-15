8a987629587f5addb31e7ab45fed141216be63e7
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2023-12-15 16:48:06 +0530

    Added func and updated struct
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

--- Go-workspace/src/Go-training/10_Structs/main.go
+++ Go-workspace/src/Go-training/10_Structs/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
-	"go-training/10_Structs/struct"
+	struct_test "go-training/10_Structs/struct"
 )
 
 type person struct {
 	first string
 	last string
 	age int
+}
+
+type secretAgent struct {
+	admi person
+	ltk bool
 }
 
 func main() {
@@ -20,7 +25,7 @@ func main() {
 		last: "Bond",
 		age: 32,
 	}
-
+ 
 	p2 := person {
 		first: "James",
 		last: "Gatling",
@@ -41,6 +46,8 @@ func main() {
 
 	fmt.Printf("%T\t %#v\n",p1,p1)
 
+
+	// Embedded struct
 	m1 := struct_test.Man{
 		First_name: "Monkey d",
 		Last_name: "luffy",
@@ -50,5 +57,52 @@ func main() {
 
 
 	fmt.Printf("%T\t%#v\n",m1,m1)
-	
+
+	var sa1 secretAgent
+
+	sa1 = secretAgent {
+		admi: person{
+			first: "Utkarsh",
+			last: "Singh",
+			age: 21,
+		},
+
+		ltk: true, 
+	}
+
+	Sa1 := struct_test.Secretagent{
+		Man : struct_test.Man{
+			First_name: "Utkarsh",
+			Last_name: "Singh",
+			CodeName: 007,
+		},
+
+		Ltk: true,
+	}
+
+	fmt.Printf("%T\n",Sa1.Man)
+	fmt.Printf("%T\n",sa1.admi)
+
+
+
+	// Anonymous struct
+	var a1 struct{
+		emp_id int
+		yoj int
+		salary float64
+	}
+
+	fmt.Printf("%T\n",a1)
+
+	// assign a value to a variable or identifier id1 = type{value}
+	a1 = struct{
+		emp_id int
+		yoj int
+		salary float64
+	}{
+		emp_id: 2345,
+		yoj: 2014,
+		salary: 540089.902,
+	}
+
 }
---
+++ Go-workspace/src/Go-training/11_Functions_basic/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+
+}
+
+// func (reciever) some_identifier (some_parameters some_type) (some_return) {code}
+
+
