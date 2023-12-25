522550d9c1008d8de421711eeb75f9f41a42601d
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2023-12-25 22:16:52 +0530

    Added method sets and generics
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

---
+++ Go-workspace/src/Go-training/24_Method_sets/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+
+type dog struct {
+	first string
+}
+
+
+func (d dog) walk() {
+	fmt.Println(d.first)
+}
+
+func (d *dog) run() {
+
+	d.first = "Rover"
+	fmt.Println(d.first)
+}
+
+type youngin interface {
+	walk()
+	run()
+}
+
+
+func hyper ( y youngin) {
+	y.run()
+}
+
+
+func main() {
+
+	d1 := dog{"henry"}
+
+        d2 := &dog{"janet"}
+
+
+	d1.walk()
+	d1.run()
+
+	// hyper(d1.first)
+	
+	d2.walk()
+	d2.run()
+
+	hyper(d2)
+}
