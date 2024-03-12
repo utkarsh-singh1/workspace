f2edd45e2198e838f82c522541cdc2e192629377
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2024-03-12 23:32:13 +0530

    Added new file server-in-go
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

---
+++ web/go/12_serversingo/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	li ,err := net.Listen("tcp", ":5050")
+	if err != nil {
+		log.Panic("Error =",err)
+	}
+
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println("Error =",err)
+		}
+
+		io.WriteString(conn, "\nHello from the tcp server\n")
+
+		fmt.Fprintln(conn, "How is your day?")
+
+		fmt.Fprintf(conn, "%v", "well i hope!\n")
+
+		conn.Close()
+	}
+}
