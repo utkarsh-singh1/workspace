c423c76291efb2fc4b6907c905ce33bd9974a535
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2024-03-20 23:30:48 +0530

    Added new file -> what is request in ServeHTTP method
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

---
+++ web/go/24_nethttprequest/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+
+type person int
+
+var tmp *template.Template
+
+func init() {
+
+	tmp = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func (p person) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln("Error =",err)
+	}
+
+	tmp.ExecuteTemplate(w, "index.gohtml", req.Form)
+}
+
+
+func main() {
+
+	var man person
+
+	http.ListenAndServe(":8080",man)
+	
+}
