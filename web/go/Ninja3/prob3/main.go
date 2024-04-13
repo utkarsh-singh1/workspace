efa75cf6a4df7af7746ace80a465b74ed61f9393
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2024-04-13 23:29:15 +0530

    Added new Practice Problems
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

--- web/go/Ninja3/prob3/main.go
+++ web/go/Ninja3/prob3/main.go
@@ -1,4 +1,4 @@
-// Prob3 
+// Prob3
 // Take the previous program and change it so that:
 // func main uses http.Handle instead of http.HandleFunc
 // Contstraint: Do not change anything outside of func main
