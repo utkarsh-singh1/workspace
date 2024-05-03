71aead14ea4cd6ba4e1837e695d815ebe5222918
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2024-05-04 00:14:44 +0530

    Added new file -> count number of users using cookies
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

---
+++ web/go/48_Countusersbasedoncookies/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+
+	http.HandleFunc("/", set)
+
+	http.HandleFunc("/favicon.ico", http.NotFound)
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+
+func set ( w http.ResponseWriter, req *http.Request) {
+
+	cookie, err := req.Cookie("cookie")
+	if err == http.ErrNoCookie {
+
+		cookie = &http.Cookie{
+			Name: "cookie",
+			Value: "0",
+		}
+
+	}
+
+	count, err := strconv.Atoi(cookie.Value)
+	if err != nil {
+
+		log.Println("error =",err)	
+	}
+
+	count++
+
+	cookie.Value = strconv.Itoa(count)
+
+	http.SetCookie(w , cookie)
+
+	io.WriteString(w , cookie.Value )
+	
+}
