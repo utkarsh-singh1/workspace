878f5b4908365a7c5aa9790ae78cc2f99ca6bd61
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2024-05-06 23:20:47 +0530

    Added new file -> create a new uuid to set a cookie for a request
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

---
+++ web/go/50_uuid/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+func main() {
+
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/favicon.ico", http.NotFound )
+	http.ListenAndServe(":8080", nil)
+	
+
+}
+
+
+func foo(w http.ResponseWriter, req *http.Request) {
+
+	c, err := req.Cookie("session")
+	if err != nil {
+		id := uuid.NewV4()
+		c = &http.Cookie{
+			Name: "session",
+			Value: id.String() ,
+		}
+
+		http.SetCookie(w, c)
+
+	}
+
+	fmt.Println(c)
+}
--- web/go/go.mod
+++ web/go/go.mod
@@ -3,3 +3,5 @@ module web-Go
 go 1.21.7
 
 require github.com/julienschmidt/httprouter v1.3.0 // direct
+
+require github.com/satori/go.uuid v1.2.0 // indirect
