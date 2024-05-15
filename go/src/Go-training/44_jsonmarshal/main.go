94c7c030b2880e16ec1e69ed56f83ef195613a3b
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2024-05-15 23:45:55 +0530

    Added new file -> json marshal and unmarshal
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

--- go/src/Go-training/44_jsonmarshal/main.go
+++ go/src/Go-training/44_jsonmarshal/main.go
@@ -1,14 +1,38 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type user struct {
 
 	Name string
 	Age int
-	loveit bool
+	Loveit string
 }
 
 func main() {
 
-	
+	u1 := user{
 
+		"utkarsh", 18, "yes",
+	}
+
+	u2 := user{
+
+		"kyle", 23, "no",
+		
+	}
+
+	u := []user{u1,u2}
+	
+	bs, err := json.Marshal(u)
+	if err != nil {
+
+		fmt.Println("Error =",err)
+	}
+
+	fmt.Println(string(bs))
+	
 }
---
+++ go/src/Go-training/45_jsonUnMarshal/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+
+	var s string
+
+	bs := []byte(s)
+
+}
