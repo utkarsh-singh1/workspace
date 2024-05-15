5c90e1e7836cab3ba2a320ff04536c9d4ba0277d
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2024-05-15 23:54:44 +0530

    Updated new file -> json Unmarshall
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

--- go/src/Go-training/45_jsonUnMarshal/main.go
+++ go/src/Go-training/45_jsonUnMarshal/main.go
@@ -1,9 +1,34 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person []struct {
+	Name   string `json:"Name"`
+	Age    int    `json:"Age"`
+	Loveit string `json:"Loveit"`
+}
+
 func main() {
 
 	var s string
 
+	s = `[{"Name":"utkarsh","Age":18,"Loveit":"yes"},{"Name":"kyle","Age":23,"Loveit":"no"}]`
+	
+
 	bs := []byte(s)
 
+	
+	ps := person{}
+
+	err := json.Unmarshal(bs, &ps)
+	if err != nil {
+
+		fmt .Println("Error =",err)
+
+	}
+
+	fmt.Println("Unmarshalled JSON ->", ps)
 }
