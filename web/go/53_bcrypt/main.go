334756584b6681ed4e1a18e6669c2957e4ffc571
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2024-07-09 01:41:49 +0530

    Added new file -> Securing Password with bcrypt
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

---
+++ web/go/53_bcrypt/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"net/http"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type user struct {
+	Username, First, Last, Password string
+}
+
+var dbSession map[string]string
+
+var dbUser map[string]user
+
+func main() {
+
+	http.HandleFunc("/", index)
+
+	http.HandleFunc("/signup", signUp)
+
+	http.HandleFunc("/bar", bar)
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+
+	getUser(w , req ) 
+
+}
+
+func signUp(w http.ResponseWriter, req *http.Request) {
+
+
+	if alreadyLoggedIn(req) {
+
+		http.Redirect(w , req , "/", http.StatusSeeOther)
+	}
+
+	// if User does not exist create one
+	if req.Method == http.MethodPost {
+
+		un := req.FormValue("Username")
+		f := req.FormValue("First")
+		l := req.FormValue("Last")
+		p := req.FormValue("Password")
+
+		// if username is already exist or not
+
+		if _, ok := dbUser[un] ; ok {
+
+			http.Error(w , "Username already exist", http.StatusForbidden)
+			return
+
+		}
+
+		// Create a session for new signUp
+
+		c := &http.Cookie{Name: "session", Value: uuid.NewV4().String()}
+		http.SetCookie(w , c)
+
+		// Attach session to a Username
+
+		dbSession[c.Value] = un
+
+		// Add userdetail to username
+
+		dbUser[un] = user{un,f,l,p}
+		
+	}
+	
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+
+	u := getUser(w , req )
+
+	if !(alreadyLoggedIn(req)) {
+
+		http.Redirect(w , req , "/", http.StatusSeeOther)	
+	}
+
+	
+	
+}
--- web/go/go.mod
+++ web/go/go.mod
@@ -4,4 +4,7 @@ go 1.21.7
 
 require github.com/julienschmidt/httprouter v1.3.0 // direct
 
-require github.com/satori/go.uuid v1.2.0 // indirect
+require (
+	github.com/satori/go.uuid v1.2.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+)
