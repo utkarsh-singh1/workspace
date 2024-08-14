aae6f0d6edcbfe9fab4d64377fc4d62e24158e1b
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2024-08-14 22:08:58 +0530

    Added new tutorials
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

---
+++ lua/training/chunks/dofile.lua
@@ -0,0 +1,10 @@
+function norm (x, y)
+	local n2 = x^2 + y^2
+	return math.sqrt(n2)
+end
+
+
+function double (n)
+	return 2*n
+end
+
---
+++ lua/training/variables/global-variables.lua
@@ -0,0 +1,11 @@
+
+
+print(b)
+
+-- identifier which has not had any value assigned to it will not result in any error just its value will be nil
+
+b = 1
+
+print(b)
+
+-- if variable life is to keep short use local variable but if wanna delete a global variable just assign it a nil, a global variable will only exist if it has a non-nil value. 
