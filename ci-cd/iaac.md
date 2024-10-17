53c90acd42f44b3da811c6d65aa56a88db30b045
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2024-10-17 22:40:39 +0530

    Added iac and concurrency
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

---
+++ ci-cd/iaac.md
@@ -0,0 +1,13 @@
+# Infrastructure as a Code
+
+## What It is 
+
+* Describe your infrastructure in a textual format so that system can create that desired configueation.
+* Not just any text file . an textual code (in genral YAML format) that can given to IaC tool so that it can provision your infrastructure element.
+
+## Why IaC
+
+* Reduce manual configs and software configs.
+* **CMSs ( Software Management System)** made this possible.
+* Since textual code is still available, infrastructure can be rapidly created and across different plateforms it can be repeatadly created. 
+
--- go/src/Go-training/concurrency_and_paralleism/main.go
+++ go/src/Go-training/concurrency_and_paralleism/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
+
+
+var wg sync.WaitGroup
 
 func main() {
 
@@ -15,27 +19,30 @@ func main() {
 	
 	fmt.Println("OS\t\t",os)
 	fmt.Println("Arch\t\t",arch)
-	fmt.Println("CPUS\t\t",cpus)
-	fmt.Println("Goroutine\t",goroun)
+	fmt.Println("Intial CPUS\t\t",cpus)
+	fmt.Println("Intial Goroutine\t",goroun)
 
 
-
+	wg.Add(1)
 	go foo()
 	bar()
-	
 
 	fmt.Println("Number of CPUS\t\t",cpus)
 	fmt.Println("Number of Goroutines\t" ,goroun)
-
+	
+	wg.Wait()
 }
 
 func foo() {
-
-	fmt.Println(1)
+	for i := 0 ; i < 10 ; i++{
+		fmt.Println(i)
+	}
+	wg.Done()
 }
 
 func bar() {
-
-	fmt.Println(2)
+	for j := 0 ; j < 10 ; j++{
+		fmt.Println(j)
+	}
 }
 
