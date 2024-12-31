4765919eb73333d574d20fecfded43a80d7453e5
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2024-12-31 18:38:24 +0530

    Added new properties to the bookAPI and added its description
    Signed-off-by: utkarsh-singh1 <utkarsh.singh1@india.nec.com>

--- api/projects/bookApI/bookAPI/api.go
+++ api/projects/bookApI/bookAPI/api.go
@@ -1,6 +1,9 @@
 package bookapi
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Book struct {
 	ID     int
@@ -25,19 +28,70 @@ func new() *BookDb {
 	return bd
 }
 
-func (bs *BookDb) CreateEntry(Name string, Author string, Date time.Time) int {
+func (bd *BookDb) CreateEntry(Name string, Author string, Date time.Time) int {
 
 	b := Book{
-		ID:     bs.Id,
+		ID:     bd.Id,
 		Name:   Name,
 		Author: Author,
 		Date:   Date,
 	}
 
-	bs.Bookmap[bs.Id] = b
+	bd.Bookmap[bd.Id] = b
 
-	bs.Id++
+	bd.Id++
 
 	return b.ID
 
+}
+
+func (bd *BookDb) DeleteEntry(id int) {
+
+	delete(bd.Bookmap, id)
+
+}
+
+func (bd *BookDb) DeleteAll() {
+
+	bd.Bookmap = make(map[int]Book)
+
+}
+
+func (bd *BookDb) GetById(id int) (Book, error) {
+
+	if book, err := bd.Bookmap[id] ; !err {
+
+		return book, nil
+	}else {
+		return Book{}, fmt.Errorf("This book with this id -%v does not Exist",id)
+
+	}
+}
+
+func (bd *BookDb) GetAllEntry() []Book{
+
+	allBook := make([]Book, 0, len(bd.Bookmap))
+	
+	for _, book := range bd.Bookmap {
+
+		allBook = append(allBook, book)
+
+	}
+
+	return allBook
+}
+
+func (bd *BookDb) GetByAuthor(author string) []Book {
+
+	allBook := make([]Book, 0, len(bd.Bookmap))
+
+	for _, book := range bd.Bookmap {
+
+		if book.Author == author {
+
+			allBook = append(allBook, book)
+		}
+	}
+
+	return allBook
 }
